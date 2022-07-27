1707826b7f9d3b3764e89da36ba6a076270af432
unknown <87032623+Yagoviana186@users.noreply.github.com>
2022-07-27 11:57:22 -0300

    Projeto da Digitalinnovation

---
+++ desafio-poo-dio-master/src/devYago.java
@@ -0,0 +1,20 @@
+
+public enum devYago {
+    ;
+
+    public static String getConteudosInscritos() {
+        return null;
+    }
+
+    public static String getConteudosConcluidos() {
+        return null;
+    }
+
+    public static void progredir() {
+    }
+
+    public static String calcularTotalXp() {
+        return null;
+    }
+
+}
